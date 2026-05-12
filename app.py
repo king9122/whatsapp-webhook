cdadc5f623d7c5dd4cf12b8a35818d90c8be80f9
king9122 <shykumar9@gmail.com>
2026-05-13 01:37:42 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+VERIFY_TOKEN = "mytoken2024"
+
+@app.route("/webhook", methods=["GET"])
+def verify():
+    if request.args.get("hub.verify_token") == VERIFY_TOKEN:
+        return request.args.get("hub.challenge"), 200
+    return "Error", 403
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    return "OK", 200
+
+@app.route("/")
+def home():
+    return "Server chal raha hai!", 200
